f2546663f20e9c60ba5487c9d738f72acb596b3a
omar abu elezz <miro_ronaldinho10@hotmail.com>
2018-12-16 23:45:49 +0200

    db insertion

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 include("config.php");
-$sql="Insert into userdata (ID, name , email, password) values
+$sql="Insert into userdata (username ,name,  email, password) values
 ('$_POST[username]','$_POST[name]','$_POST[email]','$_POST[password]')";
 
 if(! mysqli_query($con,$sql))
