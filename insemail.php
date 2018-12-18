02cf06b08622fe06b34a830d7f2d92c17bbd8de9
KhaledYazdii <kedo_08@hotmail.com>
2018-12-18 04:25:02 +0200

    Subscription db

---
+++ insemail.php
@@ -0,0 +1,13 @@
+<?php
+
+include ("config.php");
+$sql="insert into subscription(email) value('$_POST[email]')";
+if (!mysqli_query($con,$sql))
+{
+	echo "Error: ".mysql_error($con);
+}
+else
+{
+	header( "Location: http://localhost:8888/Gorilla%20Brand/index.html" );
+}
+?>
