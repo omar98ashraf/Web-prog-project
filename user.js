145a534ba5906266e4ea92462c32823273d43f9e
KhaledYazdii <kedo_08@hotmail.com>
2018-12-18 02:59:35 +0200

    Signup validation

---
+++ user.js
@@ -0,0 +1,24 @@
+function process(){
+
+    name = document.getElementById("uname").value;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET","user.php?name=" + name,true);
+
+    xhr.onreadystatechange = function () {
+
+     if( xhr.readyState == 4  && xhr.status == 200)
+     {
+         xmlResponse= xhr.responseXML;  //returns a Document containing the HTML or XML retrieved by the request
+         XMLDocument=xmlResponse.documentElement; // returns the Element that is the root element of the document (for example, the html element for HTML documents).
+         message=XMLDocument.firstChild.data; // returns the first child node of the XML root file
+
+         document.getElementById("underInput").innerHTML='<span style=color:red> '+message+'</span>';
+
+              }
+}
+    xhr.send(null);
+     setTimeout('process()',1000);
+
+}
---
+++ user.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/xml; charset=utf-8');
+
+echo '<response>';
+$name= $_GET['name'];
+$con = mysqli_connect('localhost','root','root') or die(mysqli_error());
+mysqli_select_db($con,'gorilla_brand') or die("cannot select DB");
+$query = mysqli_query($con,"SELECT * FROM user WHERE username = '".$name."'");
+$numrows = mysqli_num_rows($query);
+        while($row = mysqli_fetch_assoc($query))
+        {
+          $dbusername = $row['username'];
+        }
+        if($name=='')
+          echo " Enter username";
+        elseif($name == $dbusername)
+            echo $name." is not available";
+        else
+            echo $name." is available";
+
+echo '</response>';
+?>
